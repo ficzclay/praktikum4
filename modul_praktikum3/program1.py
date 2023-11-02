65bc772e8a911e46bbb50105bdadd9e5d3c38afe
“ficzclay” <ficzclay@gmail.com>
2023-11-03 02:06:35 +0700

    first commit

--- modul_praktikum3/program1.py
+++ modul_praktikum3/program1.py
@@ -1,35 +1,14 @@
 modal_awal = 100000000  # Modal awal 100 juta
-total_keuntungan = 0
+keuntungan = 0  # Inisialisasi keuntungan
 
 for bulan in range(1, 9):
-    if bulan in [3, 4]:
-        keuntungan = modal_awal * 1 / 100  # Laba 1% pada bulan ke-3 dan ke-4
+    if bulan == 3:
+        keuntungan += modal_awal * 0.01  # Laba 1% pada bulan ke-3
     elif bulan == 5:
-        keuntungan = modal_awal * 5 / 100  # Laba 5% pada bulan ke-5
+        keuntungan += modal_awal * 0.05  # Laba 5% pada bulan ke-5
     elif bulan == 8:
-        keuntungan = modal_awal * 2 / 100  # Penurunan laba 2% pada bulan ke-8
-    else:
-        keuntungan = 0  # Bulan lainnya belum mendapatkan laba
+        keuntungan += modal_awal * 0.03  # Laba 3% pada bulan ke-8
 
-    total_keuntungan += keuntungan
-    modal_awal += keuntungan
+total_keuntungan = modal_awal + keuntungan
 
-modal_awal = 100000000  # Modal awal sebesar 100 juta
-total_laba = 0  # Inisialisasi total laba
-
-for bulan in range(1, 9):
-    if bulan <= 2:
-        laba = 0  # Bulan 1 dan 2, laba 0%
-    elif bulan == 3:
-        laba = 0.01 * modal_awal  # Bulan 3, laba 1%
-    elif bulan == 5:
-        laba = 0.05 * modal_awal  # Bulan 5, laba 5%
-    elif bulan == 8:
-        laba = 0.03 * modal_awal  # Bulan 8, laba 3%
-    else:
-        laba = 0.02 * modal_awal  # Bulan-bulan lain, laba 2%
-
-    total_laba += laba  # Menambahkan laba bulan ini ke total laba
-    print(f'laba bulan ke- {bulan} sebesar: {laba}')
-
-print(f'Total laba adalah: {total_laba}')
+print("Total keuntungan selama 8 bulan adalah:", total_keuntungan)
